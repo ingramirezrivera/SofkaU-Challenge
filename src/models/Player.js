1470917f2caf947db2874801095fc6d525885387
ingramirezrivera <ing.ramirezrivera@gmail.com>
2022-05-12 17:05:03 -0500

    building the class Player

---
+++ src/models/Player.js
@@ -0,0 +1,11 @@
+export class Player {
+    /**
+     * 
+     * @param {string} name name of the player 
+     * @param {string} email email of the player 
+     */
+    constructor(playerName){
+        this.playerName = playerName;
+        
+    }
+}
