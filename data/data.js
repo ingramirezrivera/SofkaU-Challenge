f17097b49391a5f35502fb621a37c3a73165bb2e
ingramirezrivera <ing.ramirezrivera@gmail.com>
2022-05-12 10:05:49 -0500

    Building the data questions

---
+++ data/data.js
@@ -0,0 +1,293 @@
+const data = [
+    {
+        question: 'What year was the very first model of the iPhone released?',
+        category: 'Technology',
+        choices: [
+            '2009',
+            '1999',
+            '2000',
+            '2007',
+        ],
+        answer: '2007'
+    },
+    {
+        question: 'What’s the shortcut for the “copy” function on most computers?',
+        category: 'Technology',
+        choices: [
+            'Crtl + o',
+            'Crtl + c',
+            'Ctrl + h',
+            'Ctrl + i',
+        ],
+        answer: 'Crtl + c'
+    },
+    {
+        question: 'What is often seen as the smallest unit of memory?',
+        category: 'Technology',
+        choices: [
+            'Kilobyte',
+            'Kilometers',
+            'Kilobit',
+            'Kilo',
+        ],
+        answer: 'Kilobyte'
+    },
+    {
+        question: 'Is Java a porgramming languages',
+        category: 'Technology',
+        choices: [
+            'Sometimes',
+            'Yes',
+            'No',
+            'Maybe',
+        ],
+        answer: 'Yes'
+    },
+    {
+        question: 'What does HTTP stand for?',
+        category: 'Technology',
+        choices: [
+            'Hyper Table Transfer Promise',
+            'Hyper Text Transfer Permision',
+            'Hyper Text Transfer Protocol',
+            'None of the above',
+        ],
+        answer: 'Hyper Text Transfer Protocol'
+    },
+
+
+
+    {
+        question: 'Who discovered penicillin?',
+        category: 'Science',
+        choices: [
+            'Alexander Fleming',
+            'Virgina Rossemberg',
+            'Pasteur',
+            'Richard Manson',
+        ],
+        answer: 'Alexander Fleming'
+    },
+    {
+        question: 'What part of the atom has no electric charge?',
+        category: 'Science',
+        choices: [
+            'Proton',
+            'Electron',
+            'Neutron',
+            'None of the above',
+        ],
+        answer: 'Neutron'
+    },   
+    {
+        question: 'What is the symbol for Potassium',
+        category: 'Science',
+        choices: [
+            'P',
+            'K',
+            'Po',
+            'Ko',
+        ],
+        answer: 'K'
+    },
+    {
+        question: 'Which planet has the most gravity?',
+        category: 'Science',
+        choices: [
+            'Earth',
+            'Mars',
+            'June',
+            'Jupiter',
+        ],
+        answer: 'Jupiter'
+    },
+    {
+        question: 'How many molecules of oxygen does ozone have?',
+        category: 'Science',
+        choices: [
+            'Two',
+            'Eleven',
+            'One',
+            'Three',
+        ],
+        answer: 'Three'
+    },
+
+
+
+
+    {
+        question: 'What is Hawkeye’s real name?',
+        category: 'Culture',
+        choices: [
+            'Clint Barton',
+            'Jonas Priston',
+            'Dr. Jenkins Louis',
+            'Gregory Panadan',
+        ],
+        answer: 'Clint Barton'
+    },
+    {
+        question: 'Who played Wolverine?',
+        category: 'Culture',
+        choices: [
+            'Milton Church',
+            'De Jager',
+            'Hugh Jackman',
+            'None of the above',
+        ],
+        answer: 'Hugh Jackman'
+    },
+    {
+        question: 'In which US city is Broadway located?',
+        category: 'Culture',
+        choices: [
+            'New York City',
+            'Kansas City',
+            'Chicago',
+            'San Francisco',
+        ],
+        answer: 'New York City'
+    },
+    {
+        question: 'Who is often credited with creating the world’s first car?',
+        category: 'Culture',
+        choices: [
+            'Henrry Ford',
+            'Karl Benz',
+            'Kiichiro Toyoda',
+            'Enzo Ferrari',
+        ],
+        answer: 'Karl Benz'
+    },
+    {
+        question: 'Which country invented tea?',
+        category: 'Culture',
+        choices: [
+            'Japan',
+            'Spain',
+            'Germany',
+            'China',
+        ],
+        answer: 'China'
+    },
+    
+
+
+
+
+    {
+        question: 'What is the smallest country in the world?',
+        category: 'Hystory and Geography',
+        choices: [
+            'Vatican City',
+            'Bolivia',
+            'Poland',
+            'Cuba',
+        ],
+        answer: 'Vatican City'
+    },
+    {
+        question: 'In which country Adolph Hitler was born?',
+        category: 'Hystory and Geography',
+        choices: [
+            'Germany',
+            'Poland',
+            'Austria',
+            'Russia',
+        ],
+        answer: 'Austria'
+    },
+    {
+        question: 'John F. Kennedy was assassinated in which city?',
+        category: 'Hystory and Geography',
+        choices: [
+            'Kansas City',
+            'Dallas City',
+            'New York City',
+            'Chicago City',
+        ],
+        answer: 'Dallas City'
+    },
+    {
+        question: 'Which country did AC/DC originate in?',
+        category: 'Hystory and Geography',
+        choices: [
+            'Australia',
+            'Rumania',
+            'Austria',
+            'Egipt',
+        ],
+        answer: 'Australia'
+    },
+    {
+        question: 'Who was the messenger of the gods?',
+        category: 'History and Geography',
+        choices: [
+            'Hercules',
+            'Odisea',
+            'Ares',
+            'Hermes',
+        ],
+        answer: 'Three'
+    },
+
+
+
+    {
+        question: 'What is the smallest country in the world?',
+        category: 'Business & Economy',
+        choices: [
+            'Elon Musk',
+            'Bill Gates',
+            'Jeff Bezos',
+            'Jack Ma',
+        ],
+        answer: 'Elon Musk'
+    },
+    {
+        question: 'Richard Branson is the owner of which airline?',
+        category: 'Business & Economy',
+        choices: [
+            'Virgin Atlantic',
+            'Briths Airways',
+            'Delta Airlines',
+            'Klm',
+        ],
+        answer: 'Virgin Atlantic'
+    },
+    {
+        question: 'What vehicle is Volkswagen best known for in the world?',
+        category: 'Business & Economy',
+        choices: [
+            'The Beetle',
+            'Mini Couper',
+            'Smally Vw',
+            'Wagon ku',
+        ],
+        answer: 'The Beetle'
+    },
+    {
+        question: 'What is the slogan of Apple Inc.?',
+        category: 'Business & Economy',
+        choices: [
+            'Think different',
+            'Make the rules',
+            'Just Do it',
+            'Simple is the meaning',
+        ],
+        answer: 'Think different'
+    },
+    {
+        question: 'Which watch company has a pointed crown as its logo?',
+        category: 'Business & Economy',
+        choices: [
+            'Corona',
+            'Rolex',
+            'Ikea',
+            'Netflix',
+        ],
+        answer: 'Rolex'
+    },
+]
+
