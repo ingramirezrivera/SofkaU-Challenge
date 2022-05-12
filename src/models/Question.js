f0a415d57973c30230762f32a0aa928aeb9ce24e
ingramirezrivera <ing.ramirezrivera@gmail.com>
2022-05-12 14:54:01 -0500

    Starting to structure the project

---
+++ src/models/Question.js
@@ -0,0 +1,11 @@
+class Question {
+
+    constructor(id, text, category, level, choice, answer){
+        this.id = id;
+        this.text = text;
+        this.category = category;
+        this.level = level;
+        this.choice = choice;
+        this.answer = answer;
+    }
+}
